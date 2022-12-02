cd0e72e5c7d619b27d662a43406f495ef1c5fb1a
ianhurley <ian.hurley@gmail.com>
2022-12-02 12:30:21 +0000

    Blynk connection test

--- scanut.py
+++ scanut.py
@@ -1,7 +1,17 @@
+import BlynkLib
 from sense_hat import SenseHat
+from time import sleep
 
+BLYNK_AUTH = 'q1v6KTg1fFECJJ7mLuAaktu2z2SX5uPj'
+
+# initialize Blynk
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+#initialise SenseHAT
 sense = SenseHat()
 sense.clear()
+
+sense.show_message("ScaNut")
 
 r = (255,0,0)
 g = (0,255,0)
@@ -52,12 +62,20 @@ def yellow():
   sense.set_pixels(smileSad)
 
 
-
-sense.stick.direction_down = red 
 sense.stick.direction_up = green 
+sense.stick.direction_down = red 
 sense.stick.direction_left = yellow 
 sense.stick.direction_right = yellow
 sense.stick.direction_middle = sense.clear
 
+blynk.virtual_write(0, 0) #green
+blynk.virtual_write(2, 0) #yellow
+blynk.virtual_write(1, 255) #red
+
+
+# test push notification event
+blynk.log_event("scan")
+
+# infinite loop that waits for event
 while True:
-  pass
+    blynk.run()
