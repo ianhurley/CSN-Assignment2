b0352ee2ff4e9d948173562232d486f0d8fddbb6
ianhurley <ian.hurley@gmail.com>
2022-12-02 18:23:45 +0000

    Blynk/LED sync test

--- scanut.py
+++ scanut.py
@@ -11,7 +11,8 @@ blynk = BlynkLib.Blynk(BLYNK_AUTH)
 sense = SenseHat()
 sense.clear()
 
-sense.show_message("ScaNut")
+#intro
+sense.show_message("ScaNut") 
 
 r = (255,0,0)
 g = (0,255,0)
@@ -52,30 +53,34 @@ smileSad = [
   b,y,y,y,y,y,y,b
   ]
 
-def red():
-  sense.set_pixels(smileAngry)
-
-def green():
-  sense.set_pixels(smileHappy)
-
-def yellow():
-  sense.set_pixels(smileSad)
-
-
-sense.stick.direction_up = green 
-sense.stick.direction_down = red 
-sense.stick.direction_left = yellow 
-sense.stick.direction_right = yellow
-sense.stick.direction_middle = sense.clear
-
-blynk.virtual_write(0, 0) #green
-blynk.virtual_write(2, 0) #yellow
-blynk.virtual_write(1, 255) #red
-
-
-# test push notification event
-blynk.log_event("scan")
-
-# infinite loop that waits for event
+sense.clear()
 while True:
     blynk.run()
+    for event in sense.stick.get_events():
+    # Check if the joystick was pressed
+        if event.action == "pressed":
+      
+      # Check which direction
+            if event.direction == "up":
+                sense.set_pixels(smileHappy)     # Up arrow
+                blynk.virtual_write(0, 255) #green
+                blynk.virtual_write(2, 0) #yellow
+                blynk.virtual_write(1, 0) #red
+            elif event.direction == "down":
+                sense.set_pixels(smileAngry)      # Down arrow
+                blynk.virtual_write(0, 0) #green
+                blynk.virtual_write(2, 0) #yellow
+                blynk.virtual_write(1, 255) #red
+            elif event.direction == "left": 
+                sense.set_pixels(smileSad)      # Left arrow
+                blynk.virtual_write(0, 0) #green
+                blynk.virtual_write(2, 255) #yellow
+                blynk.virtual_write(1, 0) #red
+            elif event.direction == "right":
+                sense.set_pixels(smileSad)      # Right arrow
+            elif event.direction == "middle":
+                sense.show_letter("M")      # Enter key
+        
+        # Wait a while and then clear the screen
+            sleep(0.5)
+            sense.clear()
