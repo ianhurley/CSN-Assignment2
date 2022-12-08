b48c564ba6a4fffad18bcdd26b87ce68c818ee4a
ianhurley <ian.hurley@gmail.com>
2022-12-08 15:29:44 +0000

    database linkup & blynk updates

--- scanut.py
+++ scanut.py
@@ -2,6 +2,15 @@ import BlynkLib
 from sense_hat import SenseHat
 from time import sleep
 
+import pymongo
+from pymongo import MongoClient
+
+# connecting to MongoDB Atlas cloud database
+cluster = MongoClient("mongodb+srv://scanut:scanut@cluster0.x0cl6dg.mongodb.net/test")
+
+db = cluster["ScaNutDB"]
+collection = db["confectionary"]
+
 BLYNK_AUTH = 'q1v6KTg1fFECJJ7mLuAaktu2z2SX5uPj'
 
 # initialize Blynk
@@ -53,6 +62,23 @@ smileSad = [
   b,y,y,y,y,y,y,b
   ]
 
+#prints ingredients on scan for those that contain nuts
+
+def scanNuts():
+	myquery = {"_id": (barcode),"Ingredients": {'$regex': 'nut'}}
+	mydoc = collection.find(myquery)
+	for ingredients in mydoc:
+		print("CONTAINS NUTS")
+
+
+#prints ingredients on scan for those that may contain traces
+
+def scanTraces():
+	myquery = {"_id": (barcode),"May contain": {'$regex': 'nut'}}
+	mydoc = collection.find(myquery)
+	for ingredients in mydoc:
+		print("MAY CONTAIN TRACES")
+
 sense.clear()
 while True:
     blynk.run()
@@ -64,18 +90,27 @@ while True:
             if event.direction == "up":
                 sense.set_pixels(smileHappy)     # Up arrow
                 blynk.virtual_write(0, 255) #green
+                blynk.virtual_write(3, "Safe to Eat :)") #green
                 blynk.virtual_write(2, 0) #yellow
+                blynk.virtual_write(5, " ") #yellow
                 blynk.virtual_write(1, 0) #red
+                blynk.virtual_write(4, " ") #red
             elif event.direction == "down":
                 sense.set_pixels(smileAngry)      # Down arrow
                 blynk.virtual_write(0, 0) #green
+                blynk.virtual_write(3, " ") #green
                 blynk.virtual_write(2, 0) #yellow
+                blynk.virtual_write(5, " ") #yellow
                 blynk.virtual_write(1, 255) #red
+                blynk.virtual_write(4, "Contains Nuts :(") #red
             elif event.direction == "left": 
                 sense.set_pixels(smileSad)      # Left arrow
                 blynk.virtual_write(0, 0) #green
+                blynk.virtual_write(3, " ") #green
                 blynk.virtual_write(2, 255) #yellow
+                blynk.virtual_write(5, "Contains Traces :|") #yellow
                 blynk.virtual_write(1, 0) #red
+                blynk.virtual_write(4, " ") #red
             elif event.direction == "right":
                 sense.set_pixels(smileSad)      # Right arrow
             elif event.direction == "middle":
